62fc078974002995f8fdcb86a8c6343099e0754d
plaidfinch <finch@starlingcyber.net>
2024-07-15 22:21:30 -0400

    Initial commit from create-remix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "void.vote Remix application";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+            nodePackages.npm
+          ];
+
+          shellHook = ''
+            export PATH="$PWD/node_modules/.bin:$PATH"
+          '';
+        };
+      });
+}
