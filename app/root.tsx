f9b5388aa62017e9e0b19d950f709d0811533bdd
plaidfinch <finch@starlingcyber.net>
2024-07-26 20:55:15 -0400

    More mobile viewport fiddling

--- app/root.tsx
+++ app/root.tsx
@@ -21,7 +21,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
           <meta charSet="utf-8" />
           <meta
             name="viewport"
-            content="width=device-width, initial-scale=1, maximum-scale=1"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
           <link
             href="./favicon.png"
